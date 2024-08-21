581a9ae0cf79588fcac4dfeb7c93933da51c7580
grahamstockton87 <127569551+grahamstockton87@users.noreply.github.com>
2024-08-21 12:09:18 -0700

    Sky Image
    Added ray.h. main func calcs sky imagine using camera viewport directions. takes the normalized y value and applies to scaling function to blend.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <future>
 #include <algorithm>
 #include <fstream>
+#include "vec3.h"
+#include "ray.h"
 
 
 #define STBI_MSC_SECURE_CRT
@@ -19,27 +21,56 @@ bool fileExists(const char* fileName) {
     return infile.good();
 }
 
-const int image_width = 1024;
-const auto aspect_ratio = 16.0 / 9.0;
-const int image_height = static_cast<int>(image_width / aspect_ratio);
+using color = vec3; 
 
-#define CHANNEL_NUM 3
 
-uint8_t* pixels = new uint8_t[image_width * image_height * CHANNEL_NUM];
-int index = 0;
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y() + 1.0); // changes y dir to normalized length between 0 and 1
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0); // Blended value = (1-a) * startValue + a * endValue; 0 <= a <= 1
+}
 
 int main() {
 
-    // Image
-
-    auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 400;
+    const int image_width = 1024;
+    const auto aspect_ratio = 16.0 / 9.0;
 
     // Calculate the image height, and ensure that it's at least 1.
-    int image_height = static_cast<int>(image_width / aspect_ratio);
+    int image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
 
-    const char* name = "ImageOutputColors.png";
+    // Viewport widths less than one are ok since they are real valued.
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width) / image_height);
+
+#define CHANNEL_NUM 3
+
+    uint8_t* pixels = new uint8_t[image_width * image_height * CHANNEL_NUM];
+    int index = 0;
+
+    // Camera
+
+    auto focal_length = 1.0;
+    auto camera_center = point3(0, 0, 0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    auto viewport_u = vec3(viewport_width, 0, 0); // x
+    auto viewport_v = vec3(0, -viewport_height, 0); // y
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    // Calculate the location of the upper left pixel.
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+
+    // Image
+
+    
+
+    const char* name = "Sky.png";
 
     if (fileExists(name)) {
         std::cout << "File already exists. " << name << std::endl;
@@ -55,13 +86,16 @@ int main() {
         std::cerr << "\rPercent Rendered: " << static_cast <int>(100 - percent) << "% " << std::flush;
         for (int i = 0; i < image_width; ++i) {
 
-            double r = double(i) / (image_width - 1);
-            double g = double(j) / (image_height - 1);
-            double b = 0.0;
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
+
+            int ir = static_cast<int>(255.999 * pixel_color.x());
+            int ig = static_cast<int>(255.999 * pixel_color.y());
+            int ib = static_cast<int>(255.999 * pixel_color.z());
 
             pixels[index++] = ir;
             pixels[index++] = ig;
