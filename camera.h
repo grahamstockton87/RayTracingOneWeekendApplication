47a1a5ce8d3fa025e14d20f60f4a2d9bd9a12054
grahamstockton87 <grahamstockton6@gmail.com>
2025-01-01 18:05:59 -0800

    9.3

--- camera.h
+++ camera.h
@@ -27,6 +27,7 @@ public:
 	const double aspect_ratio = 16.0 / 9.0;
 	const char* image_name = "New Camera WHODIS";
 	int samples_per_pixel = 10;
+	int max_depth = 10;
 
 	void render(const hittable& world) {
 		initialize();
@@ -41,7 +42,7 @@ public:
 
 				for (int sample = 0; sample < samples_per_pixel; sample++) {
 					ray r = get_ray(i, j);
-					pixel_color += ray_color(r, world);
+					pixel_color += ray_color(r, max_depth, world);
 				}
 				pixel_color *= pixel_samples_scale;
 				// clamp rgb values [0,1} to byte range [0,255]
@@ -73,6 +74,7 @@ private:
 	point3 pixel00_loc;    // Location of pixel 0, 0
 	vec3   pixel_delta_u;  // Offset to pixel to the right
 	vec3   pixel_delta_v;  // Offset to pixel below
+
 
 	uint8_t* pixels;
 	int index;
@@ -125,12 +127,16 @@ private:
 		return vec3(random_double() - 0.5, random_double() - 0.5, 0);
 	}
 	// grabs color of ray if it intersects
-	color ray_color(const ray& r, const hittable& world) const {
+	color ray_color(const ray& r, int depth, const hittable& world) const {
+		// limit child rays 
+		if (depth <= 0)
+			return color(0, 0, 0);
+
 		hit_record rec;
 
-		if (world.hit(r, interval(0, infinity), rec)) {
+		if (world.hit(r, interval(0.001, infinity), rec)) {
 			vec3 direction = random_on_hemisphere(rec.normal); 
-			return 0.5 * ray_color(ray(rec.p,direction), world);
+			return 0.5 * ray_color(ray(rec.p,direction), depth-1, world);
 		}
 
 		vec3 unit_direction = unit_vector(r.direction());
