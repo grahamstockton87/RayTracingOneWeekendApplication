b82204ac23d1c721e0fcff89fbfedea90f8e2860
grahamstockton87 <grahamstockton6@gmail.com>
2025-01-22 13:43:28 -0800

    Dielectric

--- main.cpp
+++ main.cpp
@@ -66,7 +66,7 @@ int main() {
 
     auto material_ground = make_shared<lambertian>(color(0.0, 1.0, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.0);
+    auto material_left = make_shared<dielectric>(1.5);
     auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.7);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
@@ -76,7 +76,7 @@ int main() {
 
     //world.add(make_shared<triangle>(point3(-1.0, 1.0, -2.0), point3(1, 1.0, -2.0), point3(0.0, 1.0, 2.0), material_ground));
 
-    const char* image_name = "metalfuzz.png";
+    const char* image_name = "dielectric.png";
 
     camera cam;
     cam.image_name = image_name;
