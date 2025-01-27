4eb7051da2c6328f6866f298c3bd10a18949e255
grahamstockton87 <grahamstockton6@gmail.com>
2025-01-27 10:44:01 -0800

    fov

--- main.cpp
+++ main.cpp
@@ -66,8 +66,8 @@ int main() {
 
     auto material_ground = make_shared<lambertian>(color(0.0, 1.0, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left = make_shared<dielectric>(1.5);
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.7);
+    auto material_left = make_shared<dielectric>(1.00 / 1.33);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.2);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
@@ -76,7 +76,7 @@ int main() {
 
     //world.add(make_shared<triangle>(point3(-1.0, 1.0, -2.0), point3(1, 1.0, -2.0), point3(0.0, 1.0, 2.0), material_ground));
 
-    const char* image_name = "dielectric.png";
+    const char* image_name = "field of view.png";
 
     camera cam;
     cam.image_name = image_name;
